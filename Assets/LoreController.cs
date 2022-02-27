f19eabb91c1292cee6755668bcd000406d148145
Casey Manning <kcgrind@gmail.com>
2022-02-27 14:48:53 -0800

    better intro

--- Assets/LoreController.cs
+++ Assets/LoreController.cs
@@ -5,18 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class LoreController : MonoBehaviour
 {
+    public GameObject[] texts;
+
+    float lastChangeTime = 0;
+
+    int currentText = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        lastChangeTime = Time.time;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if ((Input.GetMouseButton(0) || Input.GetKey(KeyCode.Space)) && Time.timeSinceLevelLoad > 3)
+        if ((Input.GetMouseButton(0) || Input.GetKey(KeyCode.Space)) && Time.time - lastChangeTime > 1)
         {
-            SceneManager.LoadScene("GameScene");
+            lastChangeTime = Time.time;
+            if (currentText == texts.Length - 1)
+            {
+                SceneManager.LoadScene("GameScene");
+            } else
+            {
+                texts[currentText].SetActive(false);
+                currentText += 1;
+                texts[currentText].SetActive(true);
+            }
         }
     }
 }
